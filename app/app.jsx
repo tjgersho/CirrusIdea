63dfb61ecf6edded8aa1ea4d9263245c7b2d44e7
tjgersho <travis.g@paradigmmotion.com>
2017-05-11 18:11:48 +0000

    Working React

--- app/app.jsx
+++ app/app.jsx
@@ -1,6 +1,16 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
+var objOne = {name: "Travis", location: "Denver"};
+
+var objTwo = {
+ age: 25,
+ ...objOne
+};
+
+console.log(objTwo);
+
+
 ReactDOM.render(
   <h1>Boilerplate asdasdfasdf app!</h1>,
   document.getElementById('app')
--- public/bundle.js
+++ public/bundle.js
@@ -46,8 +46,18 @@
 
 	'use strict';
 
+	var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
+
 	var React = __webpack_require__(1);
 	var ReactDOM = __webpack_require__(158);
+
+	var objOne = { name: "Travis", location: "Denver" };
+
+	var objTwo = _extends({
+	  age: 25
+	}, objOne);
+
+	console.log(objTwo);
 
 	ReactDOM.render(React.createElement(
 	  'h1',
