baadfd3c4d4cd13b929665ed9c10d19d3a4c22a0
tjgersho <travis.g@paradigmmotion.com>
2017-05-15 17:25:00 +0000

    Adding React Router

---
+++ app/components/Examples.jsx
@@ -0,0 +1,16 @@
+var React = require('react');
+	
+var Examples = React.createClass({
+
+render: function(){
+
+return (<h1>Examples</h1>);
+
+
+}
+
+
+});
+
+
+module.exports = Examples;
---
+++ app/components/Weather.jsx
@@ -0,0 +1,16 @@
+var React = require('react');
+	
+var Weather = React.createClass({
+
+render: function(){
+
+return (<h1>Weather</h1>);
+
+
+}
+
+
+});
+
+
+module.exports = Weather;
